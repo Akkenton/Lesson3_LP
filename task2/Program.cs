fc48feab3e9094d81cebf5a2ccfb33594d85f86c
Akkenton <akkenton.msk@gmail.com>
2022-12-01 18:14:00 +0300

    Синтаксис цикла for

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Синтаксис цикла for
+
+// string Method4(int count, string text)
+// {
+//     string result = String.Empty;
+//     for (int i = 0; i < count; i++)
+//     {
+//         result = result + text;
+//     }
+//     return result;
+// }
+
+// string res = Method4(count: 10, text: "Yo!");
+// Console.WriteLine(res);
+
+for (var i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i}*{j} = {i * j}");
+    }
+    Console.WriteLine();
+}
